a511188e976ccd2164c878a1701af7e611c1ee69
ArxFelxis <176923291+ArxFelxis@users.noreply.github.com>
2025-11-08 09:46:45 +0800

    Initial setup and add basic math functions

---
+++ script.js
@@ -0,0 +1,28 @@
+// Variables to store user keyed numbers and operator
+
+let firstNum;
+let secondNum;
+let mathOp;
+
+// Basic mathematical operations 
+
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+console.log(add(1, 2))
+console.log(subtract(2, 1))
+console.log(divide(4, 2))
+console.log(multiply(1, 2))
